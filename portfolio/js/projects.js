d5dd5f0edd21d7783ded4e3816c07b32458b936d
Mohamed Ashraf <96639620+Gunblade0120@users.noreply.github.com>
2023-01-22 02:57:55 +0200

    Delete portfolio directory

--- portfolio/js/projects.js
+++
@@ -1,9 +0,0 @@
-const loader = document.getElementById(`loader`);
-
-window.addEventListener(`load`, () => {
-  setTimeout(() => {
-    loader.style.cssText = `
-  opacity: 0;
-  z-index: -1000`;
-  }, 500);
-});
